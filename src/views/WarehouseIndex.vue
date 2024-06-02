18aa50fcc04b31f186905caae38f41bdac1fb8aa
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-06-02 18:26:49 -0300

    exibindo dados da requisição numa tabela

--- src/views/WarehouseIndex.vue
+++ src/views/WarehouseIndex.vue
@@ -1,13 +1,34 @@
 <template>
   <div>
     <h1>Galpões Cadastrados</h1>
-    <button v-on:click="getWarehouses()">Carregar dados</button>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Código</th>
+          <th>Nome</th>
+          <th>Cidade</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="warehouse in warehouses" :key="warehouse.id">
+          <td>{{ warehouse.code }}</td>
+          <td>{{ warehouse.name }}</td>
+          <td>{{ warehouse.city }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
   name: "WarehouseIndex",
+  data() {
+    return {
+      warehouses: [],
+    };
+  },
   async mounted() {
     this.getWarehouses();
   },
@@ -16,12 +37,17 @@ export default {
       const response = await this.$http.get(
         "http://localhost:3000/api/v1/warehouses"
       );
-      const warehouses = await response.json();
-      console.log(warehouses);
+      this.warehouses = await response.json();
     },
   },
 };
 </script>
 
 <style scoped>
+table {
+  border: 1px solid;
+  border-collapse: collapse;
+  width: 400px;
+  margin: 0 auto;
+}
 </style>
