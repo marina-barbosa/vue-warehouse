8fcdd082242333f8e61f98c8b3ec81a81676d0b6
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-06-02 09:34:54 -0300

    add component

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="custom-button">
+    <button @click="handleClick">{{ buttonLabel }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CustomButton",
+  data() {
+    return {
+      buttonLabel: "Clique aqui",
+    };
+  },
+  methods: {
+    handleClick() {
+      this.buttonLabel = "Você clicou no botão!";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.custom-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px;
+}
+
+.custom-button button {
+  background-color: #42b883; /* Cor verde da logo do Vue.js */
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 5px;
+  transition: background-color 0.3s;
+}
+
+.custom-button button:hover {
+  background-color: #35495e; /* Cor azul escuro da logo do Vue.js */
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <CustomButton />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import CustomButton from "@/components/CustomButton.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    CustomButton,
+  },
+};
 </script>
