9db118b8c0d6c6573544b32cf48171437317ccb4
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-06-02 09:35:14 -0300

    add mode: history

--- src/router/index.js
+++ src/router/index.js
@@ -13,14 +13,12 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue')
   }
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
